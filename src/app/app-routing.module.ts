3ef8794b9a82bac50885d4523bc1894af3369564
Aleksei Kolesnikov <124324859+AlekseiKolesnikov@users.noreply.github.com>
2023-08-10 18:52:10 -0600

    Move pages to pages directory

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {HangmanLevelsComponent} from "./components/hangman/hangman-levels/hangman-levels.component";
-import {MemoryLevelsComponent} from "./components/memory/memory-levels/memory-levels.component";
-import {GameChoiceComponent} from "./components/game-choice/game-choice.component";
-import {ErrorScreenComponent} from "./components/error-screen/error-screen.component";
-import {HangmanPlaygroundComponent} from "./components/hangman/hangman-playground/hangman-playground.component";
+import {HangmanLevelsComponent} from "./pages/hangman/hangman-levels/hangman-levels.component";
+import {MemoryLevelsComponent} from "./pages/memory/memory-levels/memory-levels.component";
+import {GameChoiceComponent} from "./pages/game-choice/game-choice.component";
+import {ErrorScreenComponent} from "./pages/error-screen/error-screen.component";
+import {HangmanPlaygroundComponent} from "./pages/hangman/hangman-playground/hangman-playground.component";
 import {LoaderComponent} from "./components/ui/loader/loader.component";
 
 const routes: Routes = [
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,16 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {GameChoiceComponent} from "./components/game-choice/game-choice.component";
+import {GameChoiceComponent} from "./pages/game-choice/game-choice.component";
 import {ButtonComponent} from "./components/ui/button/button.component";
-import {HangmanLevelsComponent} from "./components/hangman/hangman-levels/hangman-levels.component";
-import {MemoryLevelsComponent} from "./components/memory/memory-levels/memory-levels.component";
-import {ErrorScreenComponent} from "./components/error-screen/error-screen.component";
-import {HangmanPlaygroundComponent} from "./components/hangman/hangman-playground/hangman-playground.component";
+import {HangmanLevelsComponent} from "./pages/hangman/hangman-levels/hangman-levels.component";
+import {MemoryLevelsComponent} from "./pages/memory/memory-levels/memory-levels.component";
+import {ErrorScreenComponent} from "./pages/error-screen/error-screen.component";
+import {HangmanPlaygroundComponent} from "./pages/hangman/hangman-playground/hangman-playground.component";
 import {LoaderComponent} from "./components/ui/loader/loader.component";
-import {ManComponent} from "./components/hangman/hangman-playground/man/man.component";
-import {WordComponent} from "./components/hangman/hangman-playground/word/word.component";
-import {AlphabetComponent} from "./components/hangman/hangman-playground/alphabet/alphabet.component";
+import {ManComponent} from "./pages/hangman/hangman-playground/man/man.component";
+import {WordComponent} from "./pages/hangman/hangman-playground/word/word.component";
+import {AlphabetComponent} from "./pages/hangman/hangman-playground/alphabet/alphabet.component";
 import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
